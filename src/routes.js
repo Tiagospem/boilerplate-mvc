36f79bcb339f096b3e384fb4961edcb99e64c49d
Tiagospem <tiagospem@gmail.com>
2019-08-20 20:28:02 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+//import authMiddleware from './app/middlewares/Auth';
+//import UserController from './app/controllers/UserController';
+//import SessionController from './app/controllers/SessionController';
+
+const routes = new Router();
+
+routes.get("/", (req, res) => {
+    return res.send("massa!");
+})
+
+//routes.post('/users', UserController.store);
+//routes.post('/sessions', SessionController.store);
+
+// aplica o middleware para as rotas abaixo
+//routes.use(authMiddleware);
+
+//routes.put('/users', UserController.update);
+
+export default routes;
